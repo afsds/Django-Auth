970758feee774d920d636131eb6e498febb79159
afsds <142832572+afsds@users.noreply.github.com>
2025-02-26 13:19:43 -0300

    Update product_viewset.py

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
@@ -11,7 +11,7 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 class ProductViewSet(ModelViewSet):
     authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+  
     serializer_class = ProductSerializer
 
     def get_queryset(self):
